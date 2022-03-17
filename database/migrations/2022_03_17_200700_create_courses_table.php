fd89674f7c4d57a8dee0607380fbeb4d26c1b5f6
Carlos Ferreira <carlos@especializati.com.br>
2022-03-17 17:11:14 -0300

    Model and migration course

---
+++ database/migrations/2022_03_17_200700_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
