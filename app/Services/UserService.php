1a38cd849006371364b6b36fbcd6b7f728b38145
Carlos Ferreira <carlos@especializati.com.br>
2022-03-29 14:33:36 -0300

    Implement User Service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -12,4 +12,29 @@ class UserService
     {
         $this->repository = $repository;
     }
+
+    public function getAll(string $filter = ''): array
+    {
+        return $this->repository->getAll($filter);
+    }
+
+    public function findById(string $id): object|null
+    {
+        return $this->repository->findById($id);
+    }
+
+    public function create(array $data): object
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update(string $id, array $data): object|null
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function delete(string $id): bool
+    {
+        return $this->repository->delete($id);
+    }
 }
