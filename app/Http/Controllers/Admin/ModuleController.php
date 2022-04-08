c8088a2263ca637d6f439d688017792c1e47fdb5
Carlos Ferreira <carlos@especializati.com.br>
2022-04-08 14:49:47 -0300

    List Modules of Course

--- app/Http/Controllers/Admin/ModuleController.php
+++ app/Http/Controllers/Admin/ModuleController.php
@@ -3,20 +3,31 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\CourseRepositoryInterface;
 use App\Repositories\ModuleRepositoryInterface;
 use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
     protected $repository;
+    protected $repositoryCourse;
 
-    public function __construct(ModuleRepositoryInterface $repository)
-    {
+    public function __construct(
+        CourseRepositoryInterface $repositoryCourse,
+        ModuleRepositoryInterface $repository
+    ) {
+        $this->repositoryCourse = $repositoryCourse;
         $this->repository = $repository;
     }
 
     public function index($courseId)
     {
-        dd($this->repository->getAllByCourseId($courseId));
+        if (!$course = $this->repositoryCourse->findById($courseId))
+            return back();
+
+        $data = $this->repository->getAllByCourseId($courseId);
+        $modules = convertItemsOfArrayToObject($data);
+
+        return view('admin/courses/modules/index-modules', compact('course', 'modules'));
     }
 }
---
+++ resources/views/admin/courses/modules/index-modules.blade.php
@@ -0,0 +1,83 @@
+@extends('admin.layouts.app')
+
+@section('title', "Módulos do Curso {$course->name}")
+
+@section('content')
+
+<h1 class="text-3xl text-black pb-6">
+    Módulos do Curso {{ $course->name }}
+    <a href="{{ route('modules.create', $course->id) }}" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        <i class="fas fa-plus"></i>
+    </a>
+</h1>
+
+<div class="w-full mt-12">
+
+    {{-- @include('admin.includes.form-search', ['routerName' => 'modules.index']) --}}
+
+    <div class="bg-white overflow-auto">
+        <table class="min-w-full leading-normal">
+            <thead>
+                <tr>
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Nome
+                    </th>
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Ações
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($modules as $module)
+                <tr>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                            <p class="text-gray-900 whitespace-no-wrap">
+                                {{ $course->name }}
+                            </p>
+                        </div>
+                    </td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <a href="{{ route('courses.show', $course->id) }}">
+                            <span
+                                class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                <span aria-hidden
+                                    class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                <span class="relative">Detalhes</span>
+                            </span>
+                        </a>
+                        <a href="{{ route('courses.edit', $course->id) }}">
+                            <span
+                                class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                                <span aria-hidden
+                                    class="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
+                                <span class="relative">Editar</span>
+                            </span>
+                        </a>
+
+                        <a href="{{ route('modules.index', $course->id) }}">
+                            <span
+                                class="relative inline-block px-3 py-1 font-semibold text-blue-900 leading-tight">
+                                <span aria-hidden
+                                    class="absolute inset-0 bg-blue-200 opacity-50 rounded-full"></span>
+                                <span class="relative">Aulas</span>
+                            </span>
+                        </a>
+                    </td>
+                </tr>
+                @empty
+                    <tr>
+                        <td colspan="1000">
+                            Sem nenhum módulo
+                        </td>
+                    </tr>
+                @endforelse
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
