9b75644c5a7e8f3838aa8faf3f839bf25afd6f0f
Carlos Ferreira <carlos@especializati.com.br>
2022-04-28 15:24:07 -0300

    List Details Support

--- resources/views/admin/supports/reply-support.blade.php
+++ resources/views/admin/supports/reply-support.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.app')
 
-@section('title', 'Dúvidas dos Alunos')
+@section('title', 'Detalhes da Dúvida')
 
 @section('content')
 
@@ -12,11 +12,13 @@
             <div class="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
             <div class="relative flex items-center space-x-4">
                 <div class="relative">
-                <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="" class="w-10 sm:w-16 h-10 sm:h-16 rounded-full">
+                <img src="{{ $support->user->image ? url("storage/{$support->user->image}") : url('images/user.png') }}" alt="" class="w-10 sm:w-16 h-10 sm:h-16 rounded-full">
                 </div>
                 <div class="flex flex-col leading-tight">
                     <div class="text-2xl mt-1 flex items-center">
-                        <span class="text-gray-700 mr-3">Anderson Vanhron</span>
+                        <span class="text-gray-700 mr-3">
+                            {{ $support->user->name }}
+                        </span>
                     </div>
                 </div>
             </div>
@@ -24,95 +26,26 @@
             <div class="flex sm:items-center justify-between py-6 border-b-2 border-gray-200">
                 <div class="relative flex items-center space-x-4">
                     <span class="text-gray-700 mr-3">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus condimentum lacus, dictum placerat ex faucibus et. Duis pharetra nisi vel tellus vehicula gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce rhoncus vitae neque malesuada ultricies. Duis in massa eros. Phasellus eget lorem ex. Morbi fermentum, ante vitae lobortis molestie, nulla tortor volutpat velit, vel viverra eros augue quis metus. Praesent sed blandit purus. Donec in arcu egestas quam rutrum pellentesque. Nunc eu pellentesque tellus, ac posuere lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer egestas, erat vitae interdum molestie, velit dolor pellentesque augue, venenatis ultrices tortor nisl a dolor. Aliquam sollicitudin libero neque, at finibus eros vulputate in. Praesent varius lectus ut dolor fermentum hendrerit.
+                        {{ $support->description }}
                     </span>
                 </div>
             </div>
             <div class="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-            <div class="chat-message">
-                <div class="flex items-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Can be verified on any platform using docker</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end justify-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-1 items-end">
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Your error message says permission denied, npm global installs must be given root privileges.</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-2">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">Command was run with root privileges. I'm sure about that.</span></div>
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I've update the description so it's more obviously now</span></div>
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">FYI https://askubuntu.com/a/700266/510172</span></div>
-                        <div>
-                        <span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
-                            Check the line above (it ends with a # so, I'm running it as root )
-                            <pre># npm install -g @vue/devtools</pre>
-                        </span>
+
+                @foreach ($support->replies as $reply)
+                    @php
+
+                    @endphp
+                    <div class="chat-message">
+                        <div class="flex items-end">
+                            <div class="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
+                                <div><span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Can be verified on any platform using docker</span></div>
+                            </div>
+                            <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
                         </div>
                     </div>
-                    <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end justify-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-1 items-end">
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Any updates on this issue? I'm getting the same error when trying to install devtools. Thanks</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-2">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Thanks for your message David. I thought I'm alone with this issue. Please, 👍 the issue to support it :)</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end justify-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-1 items-end">
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-blue-600 text-white ">Are you using sudo?</span></div>
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Run this command sudo chown -R `whoami` /Users/-/.npm-global/ then install the package globally without using sudo</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-2">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">It seems like you are from Mac OS world. There is no /Users/ folder on linux 😄</span></div>
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">I have no issue with any other packages installed with root permission globally.</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end justify-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-1 items-end">
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">yes, I have a mac. I never had issues with root permission as well, but this helped me to solve the problem</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-2">
-                </div>
-            </div>
-            <div class="chat-message">
-                <div class="flex items-end">
-                    <div class="flex flex-col space-y-2 max-w-xs mx-2 order-2 items-start">
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I get the same error on Arch Linux (also with sudo)</span></div>
-                        <div><span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I also have this issue, Here is what I was doing until now: #1076</span></div>
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">even i am facing</span></div>
-                    </div>
-                    <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">
-                </div>
-            </div>
+                @endforeach
+
             </div>
             <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
             <div class="relative flex">
