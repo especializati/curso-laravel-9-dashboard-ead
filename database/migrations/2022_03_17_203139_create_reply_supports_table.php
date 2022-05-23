dedd35bb5a196770a0ab7f50f38a94f86224c9b0
Carlos Ferreira <carlos@especializati.com.br>
2022-05-23 17:46:58 -0300

    Fix Relationships

--- database/migrations/2022_03_17_203139_create_reply_supports_table.php
+++ database/migrations/2022_03_17_203139_create_reply_supports_table.php
@@ -16,14 +16,13 @@ return new class extends Migration
         Schema::create('reply_support', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('support_id')->index();
-            $table->uuid('user_id')->nullable();
-            $table->uuid('admin_id')->nullable();
+            $table->foreign('support_id')->references('id')->on('supports');
+            $table->uuid('user_id')->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->uuid('admin_id')->index()->nullable();
+            $table->foreign('admin_id')->references('id')->on('admins');
             $table->text('description');
             $table->timestamps();
-
-            $table->foreign('support_id')
-                    ->references('id')
-                    ->on('supports');
         });
     }
 
